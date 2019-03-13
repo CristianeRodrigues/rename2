0b26183f38849f0b29a9867f251e97abaad88dbf
cristiane <cris_csr@yahoo.com.br>
2019-03-13 13:31:03 -0300

    renamed and method setA call

--- src/rename2/test/B.java
+++ src/rename2/test/B.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rename2;
+package rename2.test;
+
+import rename2.A;
 
 /**
  *
@@ -11,8 +13,10 @@ package rename2;
  */
 public class B {
       int B;
+      A ClassA;
     
     public void setB(int B){
         this.B = B;
+        ClassA.setA(B);
     }
 }
