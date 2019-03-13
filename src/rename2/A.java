fdefb340e8f1b69c19f92290d303edcc7d157955
cristiane <cris_csr@yahoo.com.br>
2019-03-13 13:22:10 -0300

    first commit

---
+++ src/rename2/A.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rename2;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class A {
+    
+    int A;
+    
+    public void setA(int A){
+        this.A = A;
+    }
+    
+}
---
+++ src/rename2/B.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rename2;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class B {
+      int B;
+    
+    public void setB(int B){
+        this.B = B;
+    }
+}
